b6c3444cdffea823f92f20127799057f9c6e97d4
s322596 <S322596@students.cdu.edu.au>
2024-10-01 14:41:59 +0930

    Add project files.

---
+++ hit339group20/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hit339group20.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
