82951a379412fe92d1999f978762499b6e43ea3b
s322596 <S322596@students.cdu.edu.au>
2024-10-13 17:34:24 +0930

    Fix calendar time

--- Anyone4Tennis/Migrations/20241013075226_initialcreate.Designer.cs
+++ Anyone4Tennis/Migrations/20241013075226_initialcreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Anyone4Tennis.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241013074300_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20241013075226_initialcreate")]
+    partial class initialcreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
